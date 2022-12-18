75f0dfea829a602a0b8ddaa05450160bac6dfeb0
aaron.haslett <aaron.haslett@gmail.com>
2022-12-18 20:26:24 +1300

    diff not working yet

--- diff.py
+++ diff.py
@@ -21,6 +21,50 @@ with open(cap_fn, "r") as f:
     cap = json.load(f)
     url = cap["log"]["entries"][0]["request"]["url"]
 
+script = """e => {
+  const styles = {...window.getComputedStyle(e)};
+  const parentStyles = {...window.getComputedStyle(e.parentElement)};
+  Object.keys(parentStyles).forEach(k => {
+    if (styles[k] == parentStyles[k]) delete styles[k];
+  });
+  return styles;
+} """
+
+def iterate_locator(locator):
+    for i in range(locator.count()):
+        el = locator.nth(i)
+        tagname = el.evaluate("e => e.tagName") 
+        if tagname.lower() not in ["style", "script", "meta"]:
+            yield el
+
+def details(path, old_element, new_element):
+    old_styles = old_element.evaluate(script)
+    new_styles = new_element.evaluate(script)
+    diff = {k:(v,new_styles[k]) for (k,v) in old_styles.items() if v!=new_styles[k]}
+    out = []
+    old_tag = old_element.evaluate("e => `<${e.tagName.toLowerCase()} class='${e.className}'>`")
+    new_tag = new_element.evaluate("e => `<${e.tagName.toLowerCase()} class='${e.className}'>`")
+    new_path = path + " => " + new_tag
+    if diff:
+        out.append({
+            "path": new_path,
+            "old_tag": old_tag,
+            "new_tag": new_tag,
+            "diff": diff,
+        })
+
+    old_children = list(iterate_locator(old_element.locator("xpath=*")))
+    new_children = list(iterate_locator(new_element.locator("xpath=*")))
+    print("path:{}  children:{}".format(new_path, [e.evaluate("e=>e.tagName") for e in old_children]))
+    if len(old_children) != len(new_children):
+        print("DOM structure changes don't work yet, path=" + new_path)
+        exit(1)
+
+    for old_sub_element, new_sub_element in zip(old_children, new_children):
+        out += details(new_path, old_sub_element, new_sub_element)
+
+    return out
+
 with sync_playwright() as p:
     browser = p.chromium.launch()
     old_page = browser.new_page()
@@ -30,7 +74,12 @@ with sync_playwright() as p:
     new_page.goto(url)
     time.sleep(5)
 
-    print(old_page.locator("body").evaluate("e => e.className"))
-    print(new_page.locator("body").evaluate("e => e.className"))
+    results = {
+        "head": details("head", old_page.locator("head"), new_page.locator("head")),
+        "body": details("body", old_page.locator("body"), new_page.locator("body"))
+    }
+
+    with open("results.json", "w") as f:
+        f.write(json.dumps(results))
 
     browser.close()
